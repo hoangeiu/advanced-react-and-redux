ab770a0fc9dcc5b3c33cf6449e3f0fdd7a1c4507
Lê Minh Hoàng <hoang.le.k3set@eiu.edu.vn>
2021-09-29 14:04:11 +0700

    implement testing App, CommentBox before configure redux, implment redux

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,5 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "../App";
+import { shallow } from "enzyme";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommontList from "components/CommentList";
 
-it("shows a comment box", () => {});
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it("shows a comment box", () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  expect(wrapped.find(CommontList).length).toEqual(1);
+});
