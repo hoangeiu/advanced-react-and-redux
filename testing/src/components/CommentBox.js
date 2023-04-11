3f9ed39e06a972c2eb54c49919bdaf37a9506d38
hoangeiu <leminhhoangk3@gmail.com>
2023-04-11 14:57:35 +0700

    testing

---
+++ testing/src/components/CommentBox.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class CommentBox extends Component {
+  state = { commnent: "" };
+
+  handleChange = (event) => {
+    this.setState({ commnent: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.saveComment(this.state.commnent);
+
+    this.setState({ commnent: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea value={this.state.commnent} onChange={this.handleChange} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
+          Fetch Comments
+        </button>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(CommentBox);
---
+++ testing/src/components/__test__/CommonList.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { mount } from "enzyme";
+import Root from "../../Root";
+import CommonList from "../CommentList";
+
+let wrapper;
+beforeEach(() => {
+  const initialState = {
+    comments: ["Comment 1", "Comment 2"],
+  };
+
+  wrapper = mount(
+    <Root initialState={initialState}>
+      <CommonList />
+    </Root>
+  );
+});
+
+afterEach(() => {
+  wrapper.unmount();
+});
+
+it("creates one LI per comment", () => {
+  expect(wrapper.find("li").length).toEqual(2);
+});
+
+it("shows the text for each comment", () => {
+  expect(wrapper.render().text()).toContain("Comment 1");
+  expect(wrapper.render().text()).toContain("Comment 2");
+});
